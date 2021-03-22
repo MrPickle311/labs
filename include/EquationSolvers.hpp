211bf3dc4ce9724fcf2c6d7d4be312d35e41066f
MrPickle311 <macmacroll@gmail.com>
2021-03-22 22:37:44 +0100

    Backup

--- include/EquationSolvers.hpp
+++ include/EquationSolvers.hpp
@@ -3,20 +3,11 @@
 #include <string>
 #include <cmath>
 #include <iostream>
-/*
-TODO:
-1. Zaimplementować metodę Jacobiego DONE
-2. Zaimplementować podrelaksację dla MJ DONE
-3. Zaimplementować metodę Gaussa-Seidla i SOR
-4. Zaimplementować MINRES
-5. Przeanalizować wyniki:
-• Ile iteracji potrzebuje każda z metod?
-• Jak wpływa na zbieżność współczynnik podrelaksacji?
-*/
 
 class Solver
 {
 private:
+    bool dynamic_relaxing_flag_; 
     size_t iteration_;
     arma::colvec right_side_vector_;
     arma::colvec solutions_vector_;
@@ -25,128 +16,45 @@ protected:
     arma::mat cooficient_matrix_;
     arma::mat preconditioner_;
 private:
-    inline bool vectorHasBadLength() const
-    {
-        return right_side_vector_.n_elem != cooficient_matrix_.n_rows;
-    }
-    void checkSystemValid() const
-    {
-        if(!cooficient_matrix_.is_square())
-            throwError(std::string{"Passed matrix is not square !"});
-        if(vectorHasBadLength())
-            throwError(std::string{"Vector length != matrix size !"});
-    }
-    inline bool isStartVectorEmpty() const
-    {
-        return solutions_vector_.empty();
-    }
-    void fillStartVectorWithZero()
-    {
-        solutions_vector_.resize(right_side_vector_.n_elem);
-        for(auto&& x_i : solutions_vector_)
-            x_i = 0;
-    }
-    void initSolver()
-    {
-        setPreconditioner();
-    }
-    inline bool isFirstIteration() const
-    {
-        return iteration_ == 0;
-    }
-    //x_n+1 = Mx + Nb
-    inline void iterationEngine()
-    {
-        solutions_vector_ += relax_ * preconditioner_ * ( right_side_vector_ - cooficient_matrix_ * solutions_vector_);
-    }
+    inline bool vectorHasBadLength() const;
+    void checkSystemValid() const;
+    inline bool isStartVectorEmpty() const;
+    void fillStartVectorWithZero();
+    void initSolver();
+    inline bool isFirstIteration() const;
+    inline void iterationEngine();
+    bool isDiagZeroVector() const;
+    inline void throwError(std::string msg) const noexcept(false);
+    bool isDiagZeroVector() const;
+    inline void throwError(std::string msg) const noexcept(false);
+    inline arma::mat getStep();
+    inline arma::mat getSubstep();
+    inline void modifyRelax();
+    inline arma::mat getNumerator() const;
 protected:
     virtual void setPreconditioner() = 0;
-    virtual void setCooficientMatrix() = 0;
-    bool isDiagZeroVector() const
-    {
-        for(auto&& e: preconditioner_)
-            if(e != 0) return false;
-        return true;
-    }
-    inline void throwError(std::string msg) const noexcept(false) 
-    {
-        throw std::runtime_error(msg);
-    }
 public:
     Solver(arma::mat const& cooficient_matrix,
            arma::colvec const& right_side_vector,
-           double relax = 1.0,
-           arma::colvec const& start_positions_vector = {}):
-        cooficient_matrix_{cooficient_matrix},
-        right_side_vector_{right_side_vector},
-        preconditioner_{},
-        iteration_{0},
-        relax_{relax}
-    {
-        checkSystemValid();
-        if(isStartVectorEmpty())
-            fillStartVectorWithZero();
-    }
+           bool enable_dynamic_relax = false,
+           double start_relax = 1.0,
+           arma::colvec const& start_positions_vector = {});
     //iteration
-    void operator() ()
-    {
-        if(isFirstIteration())
-            initSolver();
-        iterationEngine();
-        ++iteration_;
-    }
-    inline bool hasGoodPrecision(double left,double right,double precision)
-    {
-        return fabs(left-right) < precision;
-    }
-    bool isInsufficientPrecision(double precision,arma::colvec const& temp)
-    {
-        size_t vector_size {temp.n_elem};
-        size_t precise_solutions {0};
-        for(size_t i{0}; i < vector_size ; ++i)
-            if(hasGoodPrecision(temp.at(i,0),solutions_vector_.at(i,0),precision))
-                ++precise_solutions;
-        if(precise_solutions == vector_size)
-            return false; // sufficient solutions
-        return true;//bad solutions
-    }
-    void operator() (size_t count)
-    {
-        for(size_t i{0}; i < count; ++i)
-            (*this)();
-    } 
-    void operator() (double precision)
-    {
-        arma::colvec temp {solutions_vector_};
-        (*this)();
-        while (isInsufficientPrecision(precision,temp))
-        {
-            temp = solutions_vector_;
-            (*this)();
-        }
-        
-    }   
-    arma::colvec getSolutions()
-    {
-        return solutions_vector_;
-    }
+    void operator() ();
+    inline bool hasGoodPrecision(double left,double right,double precision);
+    bool isInsufficientPrecision(double precision,arma::colvec const& temp);
+    void operator() (size_t count);
+    void operator() (double precision);
+    arma::colvec getSolutions();
 };
+
+//JacobiSolver and GaussSeidelSolver also have a dynamic MINRES relax modification
 
 class JacobiSolver:
     public Solver
 {
 protected:
-    virtual void setCooficientMatrix()
-    {
-        
-    }
-    virtual void setPreconditioner()
-    {
-        //get diagonal of matrix
-        preconditioner_ =  arma::diagmat(cooficient_matrix_).i();
-        if(isDiagZeroVector())
-            throwError(std::string{"Diagonal of cooficients is zero vector !"});
-    }
+    virtual void setPreconditioner();
 public:
     using Solver::Solver;
 };
@@ -155,16 +63,7 @@ class GaussSeidelSolver:
     public Solver
 {
 protected:
-    virtual void setCooficientMatrix()
-    {
-
-    }
-    virtual void setPreconditioner()
-    {
-        //get lower triangular parts
-        preconditioner_ = arma::trimatl(cooficient_matrix_,0);
-    }
+    virtual void setPreconditioner();
 public:
     using Solver::Solver;
 };
-
---
+++ src/EquationSolvers.cpp
@@ -0,0 +1,154 @@
+#include "../include/EquationSolvers.hpp"
+
+bool Solver::vectorHasBadLength() const
+{
+    return right_side_vector_.n_elem != cooficient_matrix_.n_rows;
+}
+
+void Solver::checkSystemValid() const
+{
+    if(!cooficient_matrix_.is_square())
+        throwError(std::string{"Passed matrix is not square !"});
+    if(vectorHasBadLength())
+        throwError(std::string{"Vector length != matrix size !"});
+}
+
+bool Solver::isStartVectorEmpty() const
+{
+    return solutions_vector_.empty();
+}
+
+void Solver::fillStartVectorWithZero()
+{
+    solutions_vector_.resize(right_side_vector_.n_elem);
+    for(auto&& x_i : solutions_vector_)
+        x_i = 0;
+}
+
+void Solver::initSolver()
+{
+    setPreconditioner();
+    if(isDiagZeroVector())
+        throwError(std::string{"Diagonal of cooficients is zero vector !"});
+}
+
+bool Solver::isFirstIteration() const
+{
+    return iteration_ == 0;
+}
+
+arma::mat Solver::getSubstep() 
+{
+    return right_side_vector_ - cooficient_matrix_ * solutions_vector_;
+}
+
+arma::mat Solver::getStep()
+{
+    return preconditioner_ * getSubstep();
+}
+
+void Solver::iterationEngine()
+{
+    if(dynamic_relaxing_flag_ == true)
+        modifyRelax();
+    solutions_vector_ += relax_ *  getStep();
+}
+
+double getNumerator() const;
+inline double getEnumerator() const;
+
+void Solver::modifyRelax()
+{
+    relax_ =  ( arma::trans(getSubstep()) * cooficient_matrix_ *  getStep() ) / 
+              ( arma::trans(cooficient_matrix_ *  getStep()) * 
+                                            cooficient_matrix_ *  getStep() ) ;  
+}
+
+bool Solver::isDiagZeroVector() const
+{
+    for(auto&& e: preconditioner_)
+        if(e != 0) return false;
+    return true;
+}
+
+void Solver::throwError(std::string msg) const noexcept(false) 
+{
+    throw std::runtime_error(msg);
+}
+
+Solver::Solver(arma::mat const& cooficient_matrix,
+               arma::colvec const& right_side_vector,
+               bool enable_dynamic_relax ,
+               double start_relax,
+               arma::colvec const& start_positions_vector):
+        cooficient_matrix_{cooficient_matrix},
+        right_side_vector_{right_side_vector},
+        dynamic_relaxing_flag_{start_relax},
+        preconditioner_{},
+        iteration_{0},
+        relax_{start_relax}
+{
+    checkSystemValid();
+    if(isStartVectorEmpty())
+        fillStartVectorWithZero();
+}
+
+void Solver::operator() ()
+{
+    if(isFirstIteration())
+        initSolver();
+    iterationEngine();
+    ++iteration_;
+}
+
+bool Solver::hasGoodPrecision(double left,double right,double precision)
+{
+    return fabs(left-right) < precision;
+}
+
+bool Solver::isInsufficientPrecision(double precision,arma::colvec const& temp)
+{
+    size_t vector_size {temp.n_elem};
+    size_t precise_solutions {0};
+    for(size_t i{0}; i < vector_size ; ++i)
+        if(hasGoodPrecision(temp.at(i,0),solutions_vector_.at(i,0),precision))
+            ++precise_solutions;
+    if(precise_solutions == vector_size)
+        return false; // sufficient solutions
+    return true;//bad solutions
+}
+
+void Solver::operator() (size_t count)
+{
+    for(size_t i{0}; i < count; ++i)
+        (*this)();
+} 
+
+void Solver::operator() (double precision)
+{
+    arma::colvec temp {solutions_vector_};
+    (*this)();
+    while (isInsufficientPrecision(precision,temp))
+    {
+        temp = solutions_vector_;
+        (*this)();
+    }
+}
+
+arma::colvec Solver::getSolutions()
+{
+    return solutions_vector_;
+}
+
+void JacobiSolver::setPreconditioner()
+{
+    //get diagonal of matrix
+    preconditioner_ =  arma::diagmat(cooficient_matrix_).i();
+}
+
+void GaussSeidelSolver::setPreconditioner()
+{
+    //get lower triangular parts
+    preconditioner_ = arma::trimatl(cooficient_matrix_,0);
+    preconditioner_ = arma::inv(preconditioner_);
+}
